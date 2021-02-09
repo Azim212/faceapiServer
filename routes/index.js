d6854037fc32a464a6c98ffb4695603a3b89b0a9
Azim212 <44842575+Azim212@users.noreply.github.com>
2021-02-09 16:21:54 +0800

    adding models and faceapi js

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,9 @@
-var express = require('express');
-var router = express.Router();
+var router = require("express-promise-router")();
+var faceapi = require("face-api.js")
+var fetch = require("node-fetch");
+var path = require("path")
+
+faceapi.env.monkeyPatch({ fetch: fetch })
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
@@ -11,13 +15,29 @@ router.get('/', function (req, res, next) {
 });
 
 router.post('/face', function (req, res, next) {
-  var jsonData = {
-    "name": "Azim",
-    "adminNo": "183574a",
-    "face": req.body.face
-  }
-  // console.log(req.body.face)
-  res.json(jsonData)
+  const MODEL_URL = path.join(__dirname, '/../public/models/face-api')
+
+  async function loadModels() {
+    // loading faceapi models
+    await faceapi.nets.ssdMobilenetv1.loadFromDisk(MODEL_URL);
+    // Load the face landmark models
+    await faceapi.nets.faceLandmark68TinyNet.loadFromDisk(MODEL_URL);
+    // Load the face recognition models
+    await faceapi.nets.faceRecognitionNet.loadFromDisk(MODEL_URL);
+  };
+
+  loadModels().then(nothing => {
+
+    var jsonData = {
+      "name": "Azim",
+      "adminNo": "183574a",
+      "face": req.body.face
+    }
+    // console.log(req.body.face)
+    res.json(jsonData)
+  });
+
+
 })
 
 module.exports = router;
