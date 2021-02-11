8f9ce293c3d8ee7ced2d84ec611b98bf75bdc563
Azim212 <44842575+Azim212@users.noreply.github.com>
2021-02-11 11:16:39 +0800

    finally working. will return json obj of face descriptors

--- routes/index.js
+++ routes/index.js
@@ -5,10 +5,14 @@ var fetch = require("node-fetch");
 var path = require("path")
 const jsdom = require("jsdom")
 const { JSDOM } = jsdom;
-const { document } = (new JSDOM(`...`)).window;
-const { window } = new JSDOM(`...`);
 
-// global.window = new JSDOM(`<!DOCTYPE html><img id="myImg" src="image.png" />`, { resources: "usable", url: "file:///" + __dirname + "/" }).window;
+global.window = new JSDOM(`<!DOCTYPE html><img id="faceImg"/>`,
+  {
+    pretendToBeVisual: true,
+    resources: "usable",
+    url: "file:///" + __dirname + "/"
+  })
+  .window;
 global.document = window.document;
 global.HTMLImageElement = window.HTMLImageElement
 
@@ -37,13 +41,12 @@ router.post('/face', function (req, res, next) {
     // loading faceapi models
     await faceapi.nets.ssdMobilenetv1.loadFromDisk(MODEL_URL);
     // Load the face landmark models
-    await faceapi.nets.faceLandmark68TinyNet.loadFromDisk(MODEL_URL);
+    await faceapi.nets.faceLandmark68Net.loadFromDisk(MODEL_URL);
     // Load the face recognition models
     await faceapi.nets.faceRecognitionNet.loadFromDisk(MODEL_URL);
   };
 
   async function getFaceDescription(input) {
-    console.log("here?")
     const fullFaceDescription = await faceapi.detectSingleFace(input)
       .withFaceLandmarks()
       .withFaceDescriptor()
@@ -52,32 +55,23 @@ router.post('/face', function (req, res, next) {
       return "No faces found in this image."
     }
 
-    const faceDescriptors = [fullFaceDescription.descriptor]
+    const faceDescriptors = fullFaceDescription.descriptor
     return faceDescriptors
   }
 
   loadModels().then(nothing => {
     var image = "data:image/png;base64, " + req.body.face
-    var dom = new JSDOM('<img src="' + image + '.com">', { resources: "usable", url: "file:///path/to/images/"});
-    var input = dom.window.document.querySelector("img")
+    var imgEl = document.getElementById("faceImg")
+    imgEl.src = image
 
-    var th = document.createElement("img")
-    th.src = image
-    // console.log(input)
-    console.log(input instanceof window.HTMLImageElement)
-
-    getFaceDescription(th).then(faceDescript => {
-    console.log("or here?")
+    getFaceDescription(imgEl).then(faceDescript => {
       var jsonData = {
         "faceDescriptors": faceDescript
       }
       res.json(jsonData)
     }).catch(err => console.log(err))
-    // res.send(input)
-
 
   });
-
 
 })
 
